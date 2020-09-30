adf6c8d7b53056e3868d5b2b3e1afe4c21a2e1b7
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-30 13:06:01 -0400

    setup

---
+++ cypress/integration/feedback.spec.js
@@ -0,0 +1,25 @@
+describe("Submits a feedback form", () => {
+  before(()=>{
+    cy.visit('http://zero.webappsecurity.com/feedback.html')
+  })
+
+  it("Should fill in the name", () => {
+
+  })
+
+  it("Should fill in the email address", () => {
+
+  })
+
+  it("Should fill in the subject", () => {
+
+  })
+
+  it("Should type a nasty comment", () => {
+
+  })
+
+  it("Should submit the form", () => {
+    
+  })
+})
