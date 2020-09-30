73af8a9a82bca3f17a5ae18ceba0294d5f287d22
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-30 09:43:29 -0400

    setup

---
+++ cypress/integration/resetpass.spec.js
@@ -0,0 +1,17 @@
+describe("Tests the forgot password flow", ()=>{
+  before(()=>{
+    cy.visit('http://zero.webappsecurity.com/login.html')
+  })
+
+  it("Checks that we're on the right page", () => {
+
+  })
+
+  it("Checks that the forgot password button works", ()=> {
+
+  })
+
+  it("Fills the password reset form", () => {
+    
+  })
+})
