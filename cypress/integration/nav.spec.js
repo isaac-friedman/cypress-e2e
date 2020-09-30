4a1d6418f4cc0a9ab95dc3b1177bfbae56e99d8e
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-30 12:57:28 -0400

    navbar test complete

--- cypress/integration/nav.spec.js
+++ cypress/integration/nav.spec.js
@@ -1,21 +1,24 @@
 describe("Tests navbar links", () => {
   before(() =>{
-    cy.visit('http://zero.webappsecurity.com/login.html')
+    cy.visit('http://zero.webappsecurity.com/')
   })
 
   afterEach(() => { //go back to the homepage
-    cy.back()
+    cy.go('back')
   })
 
   it("Clicks the home link", () => {
-
+    cy.get('#homeMenu').click();
+    cy.url().should('include', 'index.html')
   })
 
   it("Clicks the Online Banking link", () => {
-
+    cy.get('#onlineBankingMenu').click()
+    cy.url().should('include', 'online-banking.html')
   })
 
   it("Clicks the Feedback link", () => {
-
+    cy.get('#feedback').click()
+    cy.url().should('include', 'feedback.html')
   })
 })
