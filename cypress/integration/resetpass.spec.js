5c9fd14382ea5f87cad52c3ee093d80cc11874c0
Isaac Friedman <isaac@isaac-friedman.com>
2020-09-30 10:07:52 -0400

    e2e test of the forgot password function

--- cypress/integration/resetpass.spec.js
+++ cypress/integration/resetpass.spec.js
@@ -4,14 +4,19 @@ describe("Tests the forgot password flow", ()=>{
   })
 
   it("Checks that we're on the right page", () => {
-
+    cy.url().should('include', 'login.html')
   })
 
   it("Checks that the forgot password button works", ()=> {
-
+    cy.get('a').contains('Forgot your password').click() //Anti pattern but the page doesn't allow us to get much better
+    cy.url().should('include', 'forgot-password.html')
   })
 
   it("Fills the password reset form", () => {
-    
+    cy.get('#user_email').type('example@example.com {enter}');
+  })
+
+  it("Should display the form-submitted message", () => {
+    cy.get('.offset3').contains("Your password will be sent")
   })
 })
